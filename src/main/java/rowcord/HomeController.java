8f4140e0bd88ecf045ea243c543c238e133cf181
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-01-16 06:11:45 -0500

    Added HomeController and static homepage resource

---
+++ src/main/java/rowcord/HomeController.java
@@ -0,0 +1,18 @@
+package rowcord;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jiawe on 1/16/2016.
+ */
+@RestController
+public class HomeController {
+
+    /*@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }*/
+
+}

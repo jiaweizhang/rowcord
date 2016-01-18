ac3fb6cd6afb52f4b55c80c0ff4e0c4cb494f5a5
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-01-18 03:29:47 -0500

    Added self role change to groups'

---
+++ src/main/java/requestdata/group/ChangeRoleData.java
@@ -0,0 +1,22 @@
+package requestdata.group;
+
+/**
+ * Created by jiawe on 1/18/2016.
+ */
+public class ChangeRoleData {
+    private String groupName;
+    private int admin;
+    private int coach;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public int getCoach() {
+        return coach;
+    }
+}
--- src/main/java/rowcord/controllers/GroupController.java
+++ src/main/java/rowcord/controllers/GroupController.java
@@ -6,6 +6,7 @@ package rowcord.controllers;
 import databases.JDBC;
 import io.jsonwebtoken.Claims;
 import org.springframework.web.bind.annotation.*;
+import requestdata.group.ChangeRoleData;
 import requestdata.group.CreateGroupData;
 import responses.StandardResponse;
 
@@ -28,6 +29,20 @@ public class GroupController {
         String email = claims.getSubject();
         String groupName = gd.getGroupName();
         return createGroupDB(email, groupName);
+    }
+
+    @RequestMapping(
+            value = "/changerole",
+            method = RequestMethod.POST,
+            headers = {"Content-type=application/json"})
+    @ResponseBody
+    public StandardResponse changeRole(@RequestBody final ChangeRoleData rd, final HttpServletRequest request) {
+        final Claims claims = (Claims) request.getAttribute("claims");
+        String email = claims.getSubject();
+        String groupName = rd.getGroupName();
+        int admin = rd.getAdmin();
+        int coach = rd.getCoach();
+        return changeRoleDB( email,  groupName,  admin,  coach);
     }
 
     private StandardResponse createGroupDB(String email, String groupName) {
@@ -57,6 +72,52 @@ public class GroupController {
 
         } catch (Exception f) {
             return new StandardResponse("error", "Failed to create group");
+        }
+    }
+
+    private StandardResponse changeRoleDB(String email, String groupName, int admin, int coach) {
+        Connection c = JDBC.connect();
+        PreparedStatement st = null;
+        try {
+            st = c.prepareStatement("SELECT 1 FROM groups WHERE groupname = ? AND email = ? AND admin = 1;");
+            st.setString(1, groupName);
+            st.setString(2, email);
+
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                st.close();
+                rs.close();
+                return new StandardResponse("error", "you are not an admin in this group");
+            }
+            st.close();
+            rs.close();
+
+            if (admin == 0) {
+                st = c.prepareStatement("SELECT 1 FROM groups WHERE groupname = ? AND email <> ? AND admin = 1;");
+                st.setString(1, groupName);
+                st.setString(2, email);
+
+                rs = st.executeQuery();
+                if (!rs.next()) {
+                    st.close();
+                    rs.close();
+                    return new StandardResponse("error", "cannot change permission - no other admins");
+                }
+                st.close();
+                rs.close();
+            }
+
+            st = c.prepareStatement("UPDATE groups SET admin = ?, coach = ? WHERE groupname = ?;");
+            st.setInt(1, admin);
+            st.setInt(2, coach);
+            st.setString(3, groupName);
+            st.executeUpdate();
+            st.close();
+            return new StandardResponse("success", "Successfully changed permissions");
+
+        } catch (Exception f) {
+            f.printStackTrace();
+            return new StandardResponse("error", "Failed to change role");
         }
     }
 }

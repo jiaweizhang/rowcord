7ef1d4dd194710803b31b0821a13ecd8377ee1e6
=Jiawei Zhang <=>
2016-01-23 01:34:10 -0500

    Added get request for public groups

---
+++ src/main/java/rowcord/data/PublicGroup.java
@@ -0,0 +1,45 @@
+package rowcord.data;
+
+import java.util.Date;
+
+/**
+ * Created by jiaweizhang on 1/23/16.
+ */
+public class PublicGroup {
+    private int groupId;
+    private String groupName;
+    private String description;
+    private Date createDate;
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}

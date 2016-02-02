48cbc9d0a23204192cde415fa4b1387c16f155fb
Alan Guo <aguo5520@gmail.com>
2016-02-02 18:48:33 -0500

    Implemented my groups.

--- src/main/resources/public/js/controllers/group.js
+++ src/main/resources/public/js/controllers/group.js
@@ -1,10 +1,16 @@
-/**
+/*
  * Created by jiawe on 1/18/2016.
  */
 
 myApp.controller('groupsController', ['httpService', '$scope', '$http', function (httpService, $scope, $http) {
 
     console.log("groups controller initialized");
+
+    $scope.sortType = 'groupName';
+    $scope.sortReverse = false;
+    $scope.searchName = '';
+
+
     httpService.getGroups().then(function (response) {
         console.log(response);
         $scope.groups = response.data.data;
@@ -41,21 +47,58 @@ myApp.controller('groupcreateController', ['httpService', '$scope', '$http', fun
 }]);
 
 myApp.controller('mygroupsController', ['httpService', '$scope', '$http', function (httpService, $scope, $http) {
+    //$scope.groupName;
+    //$scope.groupID;
+    //$scope.joinDate;
+    //$scope.isAdmin;
+    //$scope.isCoach;
+    //$scope.groups;
+    $scope.sortType = 'groupName';
+    $scope.sortReverse = false;
 
-    httpService.getMembership().then(function (response) {
-        console.log(response);
-        $scope.groups = response.data.data;
-    })
+    $scope.membership = function () {
+        httpService.getMembership().then(function (response) {
+            console.log("Error + code: " + response.error + " + " + response.code);
+            console.log(response.message);
+            $scope.groups = response.data.data.groups;
+            console.log("$scope.groups: "+$scope.groups);
+            //$scope.groupName = response.data.group.groupName;
+            //$scope.groupID = response.data.group.groupID;
+            //$scope.joinDate = response.data.group.joinDate;
+            //$scope.isAdmin = response.data.group.adminBool;
+            //$scope.isCoach = response.data.group.coachBool;
+        });
+    };
+
 
     $scope.urlEncode = function(groupName) {
         return groupName.replace(/\s+/g, '+');
+    };
+
+    $scope.unixTimeConvert = function (unix_timestamp){
+        // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp*1000);
+    // Hours part from the timestamp
+        var hours = date.getHours();
+    // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        return day + ' ' + month + ' ' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
     }
+
 }]);
 
 
 myApp.controller('groupdetailController', ['httpService', '$scope', '$http', '$routeParams', function (httpService, $scope, $http, $routeParams) {
 
-    console.log('groupdetailcontroller inisitalized');
+    console.log('groupdetailcontroller initialized');
     console.log("sending request");
     console.log($routeParams);
     var data = {
